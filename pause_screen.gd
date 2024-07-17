745c8dc23db6cbd62ea6e00a4bc8d19e87b12ef8
ThePepe141 <jose.vasquez@ing.uchile.cl>
2024-07-17 18:05:33 -0400

    Add furniture collision

--- pause_screen.gd
+++ pause_screen.gd
@@ -8,30 +8,30 @@ var _player
 
 
 func _ready() -> void:
-    hide()
+	hide()
 
 func _input(event: InputEvent) -> void:
-    if event.is_action_pressed("pause"):
-        if not _player:
-            pause.rpc(multiplayer.get_unique_id())
-        elif _player.id == multiplayer.get_unique_id():
-            unpause.rpc()
-        
+	if event.is_action_pressed("pause"):
+		if not _player:
+			pause.rpc(multiplayer.get_unique_id())
+		elif _player.id == multiplayer.get_unique_id():
+			unpause.rpc()
+		
 @rpc("any_peer", "call_local")
 func pause(player_id) -> void:
-    _player = Game.get_player(player_id)
-    get_tree().paused = true
-    player_name.text = _player.name
-    if(_player.id == multiplayer.get_unique_id()):
-        panel_container.self_modulate = Color.GREEN
-        print("hola")
-    else:
-        panel_container.self_modulate = Color.BLUE
-        print("chao")
-    show()
-    
+	_player = Game.get_player(player_id)
+	get_tree().paused = true
+	player_name.text = _player.name
+	if(_player.id == multiplayer.get_unique_id()):
+		panel_container.self_modulate = Color.GREEN
+		print("hola")
+	else:
+		panel_container.self_modulate = Color.BLUE
+		print("chao")
+	show()
+	
 @rpc("any_peer", "call_local")
 func unpause() -> void:
-    _player = null
-    get_tree().paused = false
-    hide()
+	_player = null
+	get_tree().paused = false
+	hide()
